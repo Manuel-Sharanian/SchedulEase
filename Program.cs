190c07e1348ad1802a5789d377f5352022913574
Manuel-Sharanian <manuel.sharanyan@gmail.com>
2024-08-25 12:04:22 +0400

    updated program.cs

--- Program.cs
+++ Program.cs
@@ -20,7 +20,7 @@ namespace BeautySalon
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            var connectionString = builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING") ?? throw new InvalidOperationException("Connection string 'AZURE_SQL_CONNECTIONSTRING' not found.");
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(connectionString));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
